4acde051116b58cdd834e0c0df7e7c1df66d8d24
mjk0323 <jk008532@gmail.com>
2026-06-19 17:49:08 +0900

    🎉Initial commit

---
+++ src/main/java/com/notedrop/notedrop/NotedropApplication.java
@@ -0,0 +1,13 @@
+package com.notedrop.notedrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotedropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotedropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notedrop/notedrop/NotedropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notedrop.notedrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotedropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
